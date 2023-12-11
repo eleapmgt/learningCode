a2f37e08b6817e3577e0ca225054ba8044998917
Elea Pimouguet <elea.pimouguet@gmail.com>
2023-12-11 12:57:28 +0100

    coding the change event on radios buttons

--- javaScriptTraining/openClassrooms/azerType/scripts/script.js
+++ javaScriptTraining/openClassrooms/azerType/scripts/script.js
@@ -14,27 +14,48 @@ function afficherProposition(proposition) {
 
 function lancerJeu() {
 
+  // Initialisations
   let score = 0
   let i = 0
+  let listeProposition = listeMots
 
   let btnValiderMot = document.getElementById("btnValiderMot")
   let inputEcriture = document.getElementById("inputEcriture")
-  afficherProposition(listeMots[i])
+
+  afficherProposition(listeProposition[i])
+
+  // Gestion de l'événement click sur le bouton "valider"
   btnValiderMot.addEventListener("click", () => {
     console.log(inputEcriture.value)
-    if (inputEcriture.value === listeMots[i]) {
+    if (inputEcriture.value === listeProposition[i]) {
       score++
     }
     i++
     afficherResultat(score, i)
     inputEcriture.value = ''
-    if (listeMots[i] === undefined) {
+    if (listeProposition[i] === undefined) {
       afficherProposition("Le jeu est fini.")
       btnValiderMot.disabled = true
     } else {
-      afficherProposition(listeMots[i])
+      afficherProposition(listeProposition[i])
     }
   })
+
+  // Gestion de l'événement change sur les boutons radio
+  let listeBtnRadio = document.querySelectorAll(".optionSource input")
+  for (let i = 0; i < listeBtnRadio.length; i++) {
+    listeBtnRadio[i].addEventListener("change", (event) => {
+      // Si le 1er élément a été modifié, on veut jouer avec listeMots
+      if (event.target.value === "1") {
+        listeProposition = listeMots
+      } else {
+        // Sinon on veut jouer avec listePhrases
+        listeProposition = listePhrases
+      }
+      // On modifie l'affichage en direct
+      afficherProposition(listeProposition[i])
+    })
+  }
 
   afficherResultat(score, i)
 }

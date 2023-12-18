4d46f33ddde71d298ac60df096566e2e201596b2
Elea Pimouguet <elea.pimouguet@gmail.com>
2023-12-18 14:16:10 +0100

    displaying different product sheets using the for loop

--- javaScriptTraining/openClassrooms/exo-json-bonnes-pieces/pieces.js
+++ javaScriptTraining/openClassrooms/exo-json-bonnes-pieces/pieces.js
@@ -2,26 +2,33 @@
 const reponse = await fetch("pieces-autos.json");
 const pieces = await reponse.json();
 
-// Création des balises
-const article = pieces[0];
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
-const nomElement = document.createElement("h2")
-nomElement.innerText = article.nom;
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix : ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
-const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText = article.description ?? "Pas de description pour le moment."
-const stockElement = document.createElement("p");
-stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+for (let i = 0; i < pieces.length; i++) {
+  const article = pieces[i];
+  // Récupération de l'élément du DOM qui accueillera les fiches
+  const sectionFiches = document.querySelector(".fiches");
+  // Création d'une balise dédiée à une pièce automobile
+  const pieceElement = document.createElement("article");
+  // Création des balises
+  const imageElement = document.createElement("img");
+  imageElement.src = article.image;
+  const nomElement = document.createElement("h2")
+  nomElement.innerText = article.nom;
+  const prixElement = document.createElement("p");
+  prixElement.innerText = `Prix : ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+  const categorieElement = document.createElement("p");
+  categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+  const descriptionElement = document.createElement("p");
+  descriptionElement.innerText = article.description ?? "Pas de description pour le moment."
+  const stockElement = document.createElement("p");
+  stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
 
-//Rattachement de nos balises au DOM
-const sectionFiches = document.querySelector(".fiches");
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-sectionFiches.appendChild(descriptionElement);
-sectionFiches.appendChild(stockElement);
+  // Rattachement de la balise article à la section Fiches
+  sectionFiches.appendChild(pieceElement);
+  // Rattachement de nos balises au DOM
+  pieceElement.appendChild(imageElement);
+  pieceElement.appendChild(nomElement);
+  pieceElement.appendChild(prixElement);
+  pieceElement.appendChild(categorieElement);
+  pieceElement.appendChild(descriptionElement);
+  pieceElement.appendChild(stockElement);
+}
